62d9534f3a32c812abdda886b935397ded511cf3
HS <only.for.work.hs@gmail.com>
2025-04-10 15:19:09 +0800

    Test jenkinsfile 6

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Some changes for pipeline or not - test jenkinsfile 4");
+        System.out.println("Some changes for pipeline or not - test jenkinsfile 5");
     }
 
     public String getMessage() {
