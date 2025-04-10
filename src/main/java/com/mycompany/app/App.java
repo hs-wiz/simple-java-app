1d96c37942ccab7084d55ba4f61edde230c940ac
HS <only.for.work.hs@gmail.com>
2025-04-10 14:52:43 +0800

    demo webhook

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Some changes for pipeline or not");
+        System.out.println("Some changes for pipeline or not - webhook");
     }
 
     public String getMessage() {
