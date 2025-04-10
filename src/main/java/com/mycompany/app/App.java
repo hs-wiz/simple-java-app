1c09845712b22ce1fab46a982ada45bcad5e6d8e
HS <only.for.work.hs@gmail.com>
2025-04-10 15:08:57 +0800

    Test jenkinsfile 2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Some changes for pipeline or not - webhook");
+        System.out.println("Some changes for pipeline or not - test jenkinsfile");
     }
 
     public String getMessage() {
